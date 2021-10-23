338fa5af698f1244a0a4493cd966db048251d40d
Yure <91153560+YureNathan@users.noreply.github.com>
2021-10-22 21:49:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const linksSocialMedia = {
+  github: 'Yure-nathan',
+  instagram: '_yurenathan',
+  facebook: 'Yure.Nathan0101',
+  youtube: 'Yure_Nathan',
+  twitter: 'maykbrito'
+}
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGithubProfileInfos()
